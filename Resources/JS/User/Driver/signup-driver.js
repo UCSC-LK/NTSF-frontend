c907e35f8437973ae6f39bf3c22260a25237b628
AvishiGan <avishiganepola@gmail.com>
2022-12-13 22:07:47 +0530

    Re correct driver sign up js file

--- Resources/JS/User/Driver/signup-driver.js
+++ Resources/JS/User/Driver/signup-driver.js
@@ -2,20 +2,28 @@ var script = document.createElement("script");
 script.src = "https://code.jquery.com/jquery-3.6.0.min.js";
 document.getElementsByTagName("head")[0].appendChild(script);
 
-function submitSignUp() {
-  const licenceNo = document.getElementById("licenceNo");
-  const nic = document.getElementById("nic");
-  const name = document.getElementById("name");
-  const address = document.getElementById("address");
-  const email = document.getElementById("email");
-  const mobileNo = document.getElementById("mobileNo");
-  const password = document.getElementById("password");
+function submitSignup() {
+  var userType = 1; // Driver
+  const licenceNo = document.getElementById("licenceNo").value;
+  const nic = document.getElementById("nic").value;
+  const email = document.getElementById("email").value;
+  const mobileNo = document.getElementById("mobileNo").value;
+  const password = document.getElementById("password").value;
 
-  var userId = 1;
+  // const name = document.getElementById("name").value;
+  // const address = document.getElementById("address").value;
+
+  var query = $.param({
+    user_type: userType,
+    licence_no: licenceNo,
+    nic,
+    email,
+    mobile_no: mobileNo,
+    password,
+  });
 
   var settings = {
-    url: `http://localhost:8080/ntsf/signup?user_type=
-  ${userId}&nic=${nic}&email=${email}&mobile_no=${mobileNo}&password=${password}&licence_no=${licenceNo}&name=${name}&address=${address}`,
+    url: `http://localhost:8080/ntsf/signup?${query}`,
     method: "POST",
     timeout: 0,
   };
@@ -24,5 +32,6 @@ function submitSignUp() {
     console.log(response);
   });
 
-  return alert("Sign Up Successful");
+  alert("Sign Up Successful");
+  window.location.assign("#");
 }
