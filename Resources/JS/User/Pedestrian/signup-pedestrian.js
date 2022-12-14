b948feffe5b017853ce957896c864ffc30c48a61
AvishiGan <avishiganepola@gmail.com>
2022-12-14 09:55:28 +0530

    Create js file for pedestrian sign up

--- Resources/JS/User/Pedestrian/signup-pedestrian.js
+++ Resources/JS/User/Pedestrian/signup-pedestrian.js
@@ -3,19 +3,23 @@ script.src = "https://code.jquery.com/jquery-3.6.0.min.js";
 document.getElementsByTagName("head")[0].appendChild(script);
 
 function submitSignUp() {
-  const licenceNo = document.getElementById("licenceNo");
-  const nic = document.getElementById("nic");
-  const name = document.getElementById("name");
-  const address = document.getElementById("address");
-  const email = document.getElementById("email");
-  const mobileNo = document.getElementById("mobileNo");
-  const password = document.getElementById("password");
+  var userType = 3; // Pedestrian
+  // const licenceNo = document.getElementById("licenceNo");
+  const nic = document.getElementById("nic").value;
+  const email = document.getElementById("email").value;
+  const mobileNo = document.getElementById("mobileNo").value;
+  const password = document.getElementById("password").value;
 
-  var userId = 1;
+  var query = $.param({
+    user_type: userType,
+    nic,
+    email,
+    mobile_no: mobileNo,
+    password,
+  });
 
   var settings = {
-    url: `http://localhost:8080/ntsf/signup?user_type=
-  ${userId}&nic=${nic}&email=${email}&mobile_no=${mobileNo}&password=${password}&licence_no=${licenceNo}&name=${name}&address=${address}`,
+    url: `http://localhost:8080/ntsf/signup?${query}`,
     method: "POST",
     timeout: 0,
   };
@@ -24,5 +28,6 @@ function submitSignUp() {
     console.log(response);
   });
 
-  return alert("Sign Up Successful");
+  alert("Sign Up Successful");
+  window.location.assign("#");
 }
