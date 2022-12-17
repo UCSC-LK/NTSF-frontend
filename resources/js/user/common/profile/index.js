d6568ec897e64fd693d6b1ec49cac584577ebd68
AvishiGan <avishiganepola@gmail.com>
2022-12-17 12:51:20 +0530

    Add ajax to index.js file

---
+++ resources/js/user/common/profile/index.js
@@ -0,0 +1,9 @@
+var settings = {
+  url: "http://localhost:8080/ntsf/user?user_type=1&user_id=68",
+  method: "GET",
+  timeout: 0,
+};
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
