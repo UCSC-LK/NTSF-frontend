501af3979c9227ce65f152507e2c1bedc90b0383
AvishiGan <avishiganepola@gmail.com>
2022-12-17 13:06:22 +0530

    Create gotoProfile function

--- resources/js/user/common/profile/index.js
+++ resources/js/user/common/profile/index.js
@@ -1,9 +1,11 @@
-var settings = {
-  url: "http://localhost:8080/ntsf/user?user_type=1&user_id=68",
-  method: "GET",
-  timeout: 0,
-};
+function gotoProfile() {
+  var settings = {
+    url: "http://localhost:8080/ntsf/user?user_type=1&user_id=68",
+    method: "GET",
+    timeout: 0,
+  };
 
-$.ajax(settings).done(function (response) {
-  console.log(response);
-});
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+  });
+}
