c16f1a0c279002b2e7fcb94c395a41828805b5a7
Suraif Muhammad <muhammadsuraif2000@gmail.com>
2023-01-04 12:44:01 +0530

    minor changes

--- resources/js/user/driver/login.js
+++ resources/js/user/driver/login.js
@@ -1,6 +1,8 @@
+//jquery
 var script = document.createElement("script");
 script.src = "https://code.jquery.com/jquery-3.6.0.min.js";
 document.getElementsByTagName("head")[0].appendChild(script);
+
 
 function submitLogin() {
   const userType = 1; // Driver
