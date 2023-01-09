b349c51c7af9a52b2c7d4db9fd42bed36adfc641
Suraif Muhammad <muhammadsuraif2000@gmail.com>
2023-01-09 11:41:26 +0530

    fixed addpoliceman.js

--- resources/js/user/driver/addComplaint.js
+++ resources/js/user/driver/addComplaint.js
@@ -1,21 +1,34 @@
-var { sessionStorage } = require('login.js')
+// var { sessionStorage } = require('login.js')
+//jquery
+var script = document.createElement("script");
+script.src = "https://code.jquery.com/jquery-3.6.0.min.js";
+document.getElementsByTagName("head")[0].appendChild(script);
 
-
-function submitLogin()
+function addComplaint()
 {
-    const user_id = sessionStorage.user_id; 
+
+    // const user_id = sessionStorage.user_id; 
+    const user_id = document.getElementById("user_id").value;
     const title = document.getElementById("title").value;
     const description = document.getElementById("description").value;
-    
+    const complaint_no = document.getElementById("complaint_no").value;
+
+
+    console.log(user_id);
+    console.log(title);
+    console.log(description);
+    console.log(complaint_no);
+
 
     const query = $.param({
         user_id,
         title,
-        description
+        description,
+        complaint_no
     });
-
+  
     const settings = {
-        url: `http://localhost:8080/ntsf/addcomplaint?${query}`,
+        url: `http://localhost:8080/ntsf_backend_war/addcomplaint?${query}`,
         method: "POST"
     };
 
