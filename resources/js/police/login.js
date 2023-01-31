7c5ddb3340840c2d7f93953476b254d7a0198c20
Suraif Muhammad <muhammadsuraif2000@gmail.com>
2023-01-31 13:11:52 +0530

    loginAuthentication

--- resources/js/police/login.js
+++ resources/js/police/login.js
@@ -1,19 +1,58 @@
-var working = false;
-$(".login").on("submit", function (e) {
-  e.preventDefault();
-  if (working) return;
-  working = true;
-  var $this = $(this),
-    $state = $this.find("button > .state");
-  $this.addClass("loading");
-  $state.html("Authenticating");
-  setTimeout(function () {
-    $this.addClass("ok");
-    $state.html("Welcome back!");
-    setTimeout(function () {
-      $state.html("Log in");
-      $this.removeClass("ok loading");
-      working = false;
-    }, 4000);
-  }, 3000);
+form.addEventListener('submit', e => {
+	e.preventDefault();
+    loginAuthenticating();
 });
+
+function loginAuthorizing() {
+  console.log('loginAuthorizing');
+  let username = document.getElementById('username').value;
+  console.log(username);
+  let password = document.getElementById('password').value;
+  console.log(password);
+
+  if(username !=='' && password !==''){
+    loginAuthorizingBackend(username, password);
+  }
+}
+
+function loginAuthorizingBackend(username, password) {
+  console.log('loginAuthorizingBackend');
+
+  let httpReq = new XMLHttpRequest();
+
+  httpReq.onreadystatechange = function()
+  {
+    if (this.readyState == 4 && this.status == 200) 
+    {
+      if (checkloginAuthorizingBackend(this)) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+
+  httpReq.open('POST', 'http://localhost:8080/ntsf_backend_war/policemanLogin', true);
+  httpReq.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+  httpReq.send('username=' + username + '&password=' + password);
+    
+  function checkloginAuthorizingBackend(httpReq) {
+    console.log('checkloginAuthorizingBackend');
+    let jsonCheckloginAuthorizingBackendResponse = JSON.parse(httpReq.responseText);
+    console.log(jsonCheckloginAuthorizingBackendResponse);
+    let checkloginAuthorizingBackendResponseAuthorization = jsonCheckloginAuthorizingBackendResponse.authorization;
+    if (checkloginAuthorizingBackendResponseAuthorization === 'true') {
+      let checkloginAuthorizingBackendResponseRank = jsonCheckloginAuthorizingBackendResponse.rank;
+      if (checkloginAuthorizingBackendResponseRank === 'igp') {
+        window.location.href = 'http://localhost:8080/ntsf_frontend_war/igp.html';
+      } else if (checkloginAuthorizingBackendResponseRank === 'oic') {
+        window.location.href = 'http://localhost:8080/ntsf_frontend_war/oic.html';
+      } else if (checkloginAuthorizingBackendResponseRank === 'policeman') {
+        window.location.href = 'http://localhost:8080/ntsf_frontend_war/policeman.html';
+      }
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
