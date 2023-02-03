2adba4509db1ae3aea659b660b123c221c62fc01
AvishiGan <avishiganepola@gmail.com>
2023-02-03 17:05:41 +0530

    Remove userType

--- avishi_user/login/login.js
+++ avishi_user/login/login.js
@@ -5,13 +5,12 @@ document.getElementsByTagName("head")[0].appendChild(script);
 
 
 function submitLogin() {
-  const userType = 1; // Driver
-  const licenceNo = document.getElementById("licenceNo").value;
+  // const userType = 1; // Driver
+  const licenceNo = document.getElementById("nic").value;
   const password = document.getElementById("password").value;
 
   const query = $.param({
-    user_type: userType,
-    login_id: licenceNo,
+    nic: nic,
     password,
   });
 
@@ -28,7 +27,7 @@ function loginSuccessCallback(data) {
   if (data.loggedIn) {
     alert("Login successful");
     // sessionStorage.setItem("user_type", "1");
-    sessionStorage.setItem("user_id", data.userId);
+    sessionStorage.setItem("userId", data.userId);
     module.exports = {sessionStorage};
     window.location.href = "../common/fine/view.html";
   } else {
