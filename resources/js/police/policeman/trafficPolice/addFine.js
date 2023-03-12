b1b2e99fb036801dae8e8c15819942580f3f35c0
Suraif Muhammad <muhammadsuraif2000@gmail.com>
2023-03-12 23:24:46 +0530

    addFine js files made

---
+++ resources/js/police/policeman/trafficPolice/addFine.js
@@ -0,0 +1,19 @@
+
+document.getElementById('driverFine').addEventListener('click', function(){
+    console.log("driverFine button clicked");
+    let fineType = "driver";
+    sessionStorage.setItem("fineType", fineType);
+});
+
+document.getElementById('vehicleFine').addEventListener('click', function(){
+    console.log("vehicleFine button clicked");
+    let fineType = "vehicle";
+    sessionStorage.setItem("fineType", fineType);
+});
+
+document.getElementById('driverFine').addEventListener('click', function(){
+    console.log("driverFine button clicked");
+    let fineType = "driver";
+    sessionStorage.setItem("fineType", fineType);
+});
+
