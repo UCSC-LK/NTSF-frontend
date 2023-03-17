fb66766182139ce2a24d7074dc1ffd74305a1966
Suraif Muhammad <muhammadsuraif2000@gmail.com>
2023-03-17 12:09:16 +0530

    pedestrian fine addition works

--- resources/js/police/policeman/trafficPolice/addFine.js
+++ resources/js/police/policeman/trafficPolice/addFine.js
@@ -23,16 +23,12 @@ else
     console.log("Error in setting up the input coloumn for user_id based on fine_type");
 }
 
-const form = document.getElementById('form');
-const user_id = document.getElementById('user_id').value;
-console.log("user_id: " + user_id)
-const driven_vehicle = document.getElementById('driven_vehicle').value;
-console.log("driven_vehicle: " + driven_vehicle)
-const offence_no = document.getElementById('offence_no').value;
-console.log("offence_no: " + offence_no)
-const spot_description = document.getElementById('spot_description').value;
-console.log("spot_description: " + spot_description)
 
+const form = document.getElementById('form');
+const user_id = document.getElementById('user_id');
+const driven_vehicle = document.getElementById('driven_vehicle');
+const offence_no = document.getElementById('offence_no');
+const spot_description = document.getElementById('spot_description');
 
 var police_idSession = sessionStorage.getItem("user_police_id");
 var police_stationSession = sessionStorage.getItem("user_police_station");
@@ -50,9 +46,17 @@ function checkInputs() {
     
 	// trim to remove the whitespaces
     //Enter here the code to validate fine_type
-	const user_id = user_id.value.trim();
-    const offence_no = offence_no.value.trim();
-    const spot_description = spot_description.value.trim();
+    const user_idValue = user_id.value;
+    const offence_noValue = offence_no.value;
+    const spot_descriptionValue = spot_description.value;
+
+    console.log("user_idValue: " + user_idValue);
+    console.log("offence_noValue: " + offence_noValue);
+    console.log("spot_descriptionValue: " + spot_descriptionValue);
+
+	const user_idValueTrim = user_idValue.trim();
+    const offence_noValueTrim = offence_noValue.trim();
+    const spot_descriptionValueTrim = spot_descriptionValue.trim();
 
 	let flagUser_id = 0;
     let flagOffence_no = 0;
@@ -60,15 +64,15 @@ function checkInputs() {
 
     if(fine_type == "driver"){
         //user_id is license_no
-        if(user_id === '') {
+        if(user_idValueTrim === '') {
             setErrorFor(user_id, 'License No cannot be blank');
             flagUser_id = 1;
         }
-        else if((user_id.match(/^[0-9]+$/)) == null){
+        else if((user_idValueTrim.match(/^[0-9]+$/)) == null){
             setErrorFor(user_id, 'License No should contain only numbers');
             flagUser_id = 1;
         }
-        else if(user_id.length !== 10){
+        else if(user_idValueTrim.length !== 10){
             setErrorFor(user_id, 'License No should contain 10 numbers');
             flagUser_id = 1;
         }
@@ -79,19 +83,19 @@ function checkInputs() {
     }
     else if(fine_type == "vehicle"){
         //user_id is vehicle_no
-        if(user_id === '') {
+        if(user_idValueTrim === '') {
             setErrorFor(user_id, 'Vehicle No cannot be blank');
             flagUser_id = 1;
         }
-        else if((user_id.match(/^[a-zA-Z0-9]+$/)) == null){
+        else if((user_idValueTrim.match(/^[a-zA-Z0-9]+$/)) == null){
             setErrorFor(user_id, 'Vehicle No should contain only letters and numbers');
             flagUser_id = 1;
         }
-        else if(user_id.length < 6){
+        else if(user_idValueTrim.length < 6){
             setErrorFor(user_id, 'Vehicle No should contain at least 6 letters and numbers');
             flagUser_id = 1;
         }
-        else if(user_id.length > 7){
+        else if(user_idValueTrim.length > 7){
             setErrorFor(user_id, 'Vehicle No should contain at most 7 letters and numbers');
             flagUser_id = 1;
         }
@@ -102,11 +106,11 @@ function checkInputs() {
     }
     else if(fine_type == "pedestrian"){
         //user_id is nic
-        if(user_id === '') {
+        if(user_idValueTrim === '') {
             setErrorFor(user_id, 'NIC cannot be blank');
             flagUser_id = 1;
         }
-        else if((nicValidate(user_id) == false)){
+        else if((nicValidate(user_idValueTrim) == false)){
             setErrorFor(user_id, 'Invalid NIC');
             flagUser_id = 1;
         }
@@ -119,15 +123,15 @@ function checkInputs() {
         console.log("Error in validating user_id based on fine_type");
     }
 
-    if(offence_no === '') {
+    if(offence_noValueTrim === '') {
         setErrorFor(offence_no, 'Offence No cannot be blank');
         flagOffence_no = 1;
     }
-    else if((offence_no.match(/^[0-9]+$/)) == null){
+    else if((offence_noValueTrim.match(/^[0-9]+$/)) == null){
         setErrorFor(offence_no, 'Offence No should contain only numbers');
         flagOffence_no = 1;
     }
-    else if(offence_no.length !== 3){
+    else if(offence_noValueTrim.length !== 3){
         setErrorFor(offence_no, 'Offence No should contain 3 numbers, check the manual for offence no');
         flagOffence_no = 1;
     }
@@ -136,19 +140,19 @@ function checkInputs() {
         flagOffence_no = 0;
     }
 
-    if(spot_description === '') {
+    if(spot_descriptionValueTrim === '') {
         setErrorFor(spot_description, 'Spot Description cannot be blank');
         flagSpot_description = 1;
     }
-    else if((spot_description.match(/^[a-zA-Z0-9]+$/)) == null){
+    else if((spot_descriptionValueTrim.match(/^[a-zA-Z0-9]+$/)) == null){
         setErrorFor(spot_description, 'Spot Description should contain only letters and numbers');
         flagSpot_description = 1;
     }
-    else if(spot_description.length < 10){
+    else if(spot_descriptionValueTrim.length < 10){
         setErrorFor(spot_description, 'Spot Description should contain at least 10 characters');
         flagSpot_description = 1;
     }
-    else if(spot_description.length > 200){
+    else if(spot_descriptionValueTrim.length > 200){
         setErrorFor(spot_description, 'Spot Description should contain at most 200 characters');
         flagSpot_description = 1;
     }
@@ -159,8 +163,8 @@ function checkInputs() {
 
     if(flagUser_id === 0 && flagOffence_no === 0 && flagSpot_description === 0){
         console.log('came until js function for event listener of submit button');
-        console.log(user_id, offence_no, spot_description);
-        addFine(user_id, offence_no, spot_description);
+        console.log(user_idValue, offence_noValue, spot_descriptionValue);
+        addFine(user_idValue, offence_noValue, spot_descriptionValue, police_idSession, police_stationSession);
     }
     else{
         return false;
@@ -182,13 +186,13 @@ function setSuccessFor(input) {
 //Sending data to backend
 // const addFineButton = document.getElementById("addFineButton");
 
-const addFine = function(user_id, offence_no, spot_description)
+const addFine = function(user_idValue, offence_noValue, spot_descriptionValue, police_id, police_station)
 {
     console.log('came until js function for addFine which sends data to backend');
     
-    console.log("user_id: " + user_id);
-    console.log("offence_no: " + offence_no);   
-    console.log("spot_description: " + spot_description);
+    console.log("user_id: " + user_idValue);
+    console.log("offence_no: " + offence_noValue);   
+    console.log("spot_description: " + spot_descriptionValue);
 
     let httpReq = new XMLHttpRequest();
     httpReq.onreadystatechange = function()
@@ -212,7 +216,7 @@ const addFine = function(user_id, offence_no, spot_description)
     httpReq.open("POST", "http://localhost:8080/ntsf_backend_war/fine", true);
     httpReq.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     httpReq.setRequestHeader("Authorization", "Bearer " + sessionStorage.getItem('jwt'));
-    httpReq.send("action=addFine" + "&fine_type" + fine_type + "&user_id=" + user_id + "&driven_vehicle" + driven_vehicle + "&offence_no=" + offence_no + "&spot_description=" + spot_description + "&police_id=" + police_id + "&police_station=" + police_station);
+    httpReq.send("action=addFine" + "&fine_type=" + fine_type + "&user_id=" + user_idValue + "&driven_vehicle" + driven_vehicle + "&offence_no=" + offence_noValue + "&spot_description=" + spot_descriptionValue + "&police_id=" + police_id + "&police_station=" + police_station);
 
     function addFineData(httpReq)
     {
@@ -271,12 +275,12 @@ function validation(nicNumber) {
         setErrorFor(document.getElementById('nic'), 'NIC of 10 digits is valid');
     } else if (nicNumber.length === 12 && !isNaN(nicNumber)) {
         result = true;
-        console.log(nic,"NIC of 12 digits is valid");
-        setErrorFor(nic, 'NIC of 12 digits is valid');
+        console.log(nicNumber,"NIC of 12 digits is valid");
+        setErrorFor(user_id, 'NIC of 12 digits is valid');
     } else {
         result = false;
         console.log("NIC is invalid not 10 or 12");
-        setErrorFor(nic, 'NIC is invalid not 10 or 12');
+        setErrorFor(user_id, 'NIC is invalid not 10 or 12');
     }
     return result;
 }
