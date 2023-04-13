fa0c2c2737a546d572721e8af244f353c9c778bc
AvishiGan <avishiganepola@gmail.com>
2023-04-13 07:27:12 +0530

    Remove fineNo as a parameter passed to getPointsByNic function

--- user-side/service/pointSystemService.js
+++ user-side/service/pointSystemService.js
@@ -1,19 +1,19 @@
 import { HOST_NAME } from "./constants.js";
 
-export const getPointsByNic = (nic, fineNo, callback) => {
+export const getPointsByNic = (nic, callback) => {
   const httpRequest = new XMLHttpRequest();
   httpRequest.onreadystatechange = function () {
     if (this.readyState === 4) {
       const responseBody = this.responseText;
       console.log(responseBody);
       if (this.status === 200) {
-        callback(JSON.parse(responseBody), fineNo);
+        callback(JSON.parse(responseBody));
       } else {
       }
     }
   };
 
-  const queryString = `nic=${nic}&fineNo=${fineNo}`;
+  const queryString = `nic=${nic}`;
 
   httpRequest.open("GET", `${HOST_NAME}/point_system?${queryString}`, true);
   httpRequest.setRequestHeader(
