2bf9496fba7b858bd10e4d3ff874e249c9651faa
Suraif Muhammad <muhammadsuraif2000@gmail.com>
2023-04-19 20:00:49 +0530

    viewoffence.js of oic

--- resources/js/police/oic/viewOffence.js
+++ resources/js/police/oic/viewOffence.js
@@ -4,6 +4,27 @@ const loadOffenceDetails = function()
 
     console.log("I was called onload");
     console.log("Printing session storage values")
+    let offenceType = sessionStorage.getItem("offence_type");
+    console.log(offenceType);;
+
+    //Set the offence type in the page heading
+    if(offenceType === "driver")
+    {
+        document.getElementById("offenceType").innerHTML = "Driver Offences";
+    }
+    else if(offenceType === "vehicle")
+    {
+        document.getElementById("offenceType").innerHTML = "Vehicle Offences";
+    }
+    else if(offenceType === "pedestrian")
+    {
+        document.getElementById("offenceType").innerHTML = "Pedestrian Offences";
+    }
+    else{
+        console.log("Something went wrong");
+    }
+
+
     jwt = sessionStorage.getItem('jwt');
     console.log(jwt);
     user_police_id = sessionStorage.getItem('police_id');
@@ -21,7 +42,7 @@ const loadOffenceDetails = function()
     httpreq.open("POST", "http://localhost:8080/ntsf_backend_war/offence", true);
     httpreq.setRequestHeader("Content-type", "application/x-www-form-urlencoded" );
     httpreq.setRequestHeader("Authorization", "Bearer " + sessionStorage.getItem('jwt'));
-    httpreq.send("action=viewOffence");
+    httpreq.send("action=viewOffenceByType" + "&offence_type=" + offenceType);
 
     function completeLoad(httpreq)
     {
@@ -43,7 +64,7 @@ const loadOffenceDetails = function()
             let count =  jsonOffenceData.List.length - 1;
             for(i=0; i<= count; i++)
             {
-                offenceDataHTMLoutput(jsonOffenceData.List[i].offence_no, jsonOffenceData.List[i].offence_type,
+                offenceDataHTMLoutput(jsonOffenceData.List[i].offence_no,
                 jsonOffenceData.List[i].description, jsonOffenceData.List[i].amount, jsonOffenceData.List[i].demerit_points);
             }
 
@@ -56,30 +77,23 @@ const loadOffenceDetails = function()
     }
 }
 
-function offenceDataHTMLoutput(offence_no, offence_type, description, amount, demerit_points)
+function offenceDataHTMLoutput(offence_no, description, amount, demerit_points)
 {
-    console.log(offence_no);
-    console.log(offence_type);
-    console.log(description);
-    console.log(amount);
-    console.log(demerit_points);
-
     // create table data row
     var dataRow = table.insertRow();
     var dataCell1 = dataRow.insertCell(0);
     var dataCell2 = dataRow.insertCell(1);
     var dataCell3 = dataRow.insertCell(2);
     var dataCell4 = dataRow.insertCell(3);
-    var dataCell5 = dataRow.insertCell(4);
-    var dataCell6 = dataRow.insertCell(5);
-    var dataCell7 = dataRow.insertCell(6);
-
+    
     //Add content to the table data cells
     dataCell1.innerHTML = offence_no;
-    dataCell2.innerHTML = offence_type;
-    dataCell3.innerHTML = description;
-    dataCell4.innerHTML = amount;
-    dataCell5.innerHTML = demerit_points;
+    dataCell2.innerHTML = description;
+    dataCell3.innerHTML = amount;
+    dataCell4.innerHTML = demerit_points;
 }
+
+
+
 
 
