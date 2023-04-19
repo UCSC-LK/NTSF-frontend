1df0a40afcb75fc6faa9620dc9f3fa52cb9f7e1f
Suraif Muhammad <muhammadsuraif2000@gmail.com>
2023-04-19 19:54:33 +0530

    create view offence.html of oic

--- resources/js/police/oic/viewPoliceman.js
+++ resources/js/police/oic/viewPoliceman.js
@@ -84,7 +84,7 @@ function policemanDataHTMLoutput(name, police_id, nic, mobile_number, email, pos
     dataCell4.innerHTML = mobile_number;
     dataCell5.innerHTML = email;
     // dataCell6.innerHTML = position;
-
+    //not assigned should be turend as null
     if(position === "notAssigned"){
         dataCell6.innerHTML = "<select name='positionOptions' id='positionOptions' onchange='editPosition(" + police_id + ")'>" +
         "<option value='notAssigned' selected>Not Assigned</option>" + "<option value='trafficPolice'>Traffic Police</option>" + "<option value='investigationOfficer'>Investigation Officer</option>" + "<option value='courtSeargent'>Court Seargent</option>" + "</select>";
@@ -104,10 +104,6 @@ function policemanDataHTMLoutput(name, police_id, nic, mobile_number, email, pos
     else{
     }
 
-
-
-   // dataCell7.innerHTML = "<button type='button' class='btn btn-primary' id='editButton' onclick='editPosition("+police_id+")'>Update Position</button>";
-    // dataCell8.innerHTML = "<button type='button' class='btn btn-danger' id='deletebutton' onclick='deletePolicemanPopUp("+police_id+")'>Delete</button>";
 }
 
 /*Not really sure this is working pretty well (Copied from updatePoliceman) */
