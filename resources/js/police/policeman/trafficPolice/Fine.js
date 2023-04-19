e89210a78a8d5863b73032e56e659ddfe1551cce
Suraif Muhammad <muhammadsuraif2000@gmail.com>
2023-04-19 20:08:08 +0530

    remove addDriverFine.html

--- resources/js/police/policeman/trafficPolice/Fine.js
+++ resources/js/police/policeman/trafficPolice/Fine.js
@@ -1,19 +1,19 @@
 
 document.getElementById('driverFine').addEventListener('click', function(){
     console.log("driverFine button clicked");
-    let fineType = "driver";
-    sessionStorage.setItem("fine_type", fineType);
+    let offenceType = "driver";
+    sessionStorage.setItem("offence_type", offenceType);
 });
 
 document.getElementById('vehicleFine').addEventListener('click', function(){
     console.log("vehicleFine button clicked");
-    let fineType = "vehicle";
-    sessionStorage.setItem("fine_type", fineType);
+    let offenceType = "vehicle";
+    sessionStorage.setItem("offence_type", offenceType);
 });
 
 document.getElementById('pedestrianFine').addEventListener('click', function(){
     console.log("pedestrianFine button clicked");
-    let fineType = "pedestrian";
-    sessionStorage.setItem("fine_type", fineType);
+    let offenceType = "pedestrian";
+    sessionStorage.setItem("fine_type", offenceType);
 });
 
