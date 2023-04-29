1d228e731b9bb464799435aa1175006f8c129002
AvishiGan <avishiganepola@gmail.com>
2023-04-29 06:59:16 +0530

    Add validateLoginForm function and add the logic

--- user-side/user/login/script.js
+++ user-side/user/login/script.js
@@ -1,41 +1,63 @@
-/**
- * @codeoverview Below code contains the original script for the login page
- */
+import { validateNIC, validatePassword } from "/user-side/util/validator.js";
 
 // Adding jquery to the page
 var script = document.createElement("script");
 script.src = "https://code.jquery.com/jquery-3.6.0.min.js";
 document.getElementsByTagName("head")[0].appendChild(script);
 
-function submitLogin() {
+window.validateLoginForm = function validateLoginForm() {
+  // Get the whole element
+  const nicElement = document.getElementById("nic");
+  const passwordElement = document.getElementById("password");
+  const submitButtonElement = document.getElementById("submit-btn");
+
+  if (!validateNIC(nicElement.value)) {
+    console.log("Provided NIC is invalid");
+    nicElement.classList.add("invalid");
+    submitButtonElement.disabled = true;
+  } else if (!validatePassword(passwordElement.value)) {
+    passwordElement.classList.add("invalid");
+    submitButtonElement.disabled = true;
+  } else {
+    nicElement.classList.remove("invalid");
+    submitButtonElement.disabled = false;
+  }
+};
+
+export function submitLogin() {
   console.log("called");
   const nic = document.getElementById("nic").value;
   const password = document.getElementById("password").value;
 
-  const query = $.param({
-    nic,
-    password,
-  });
+  var validateStatusCode = validateParams(nic, password);
 
-  const settings = {
-    url: `http://localhost:8080/ntsf_backend_war/user_login?${query}`,
-    method: "GET",
-  };
+  switch (validateStatusCode) {
+    case 0:
+      const query = $.param({
+        nic,
+        password,
+      });
 
-  $.ajax(settings).done(loginSuccessCallback).fail(loginUnsuccessCallback);
-}
+      const settings = {
+        url: `http://localhost:8080/ntsf_backend_war/user_login?${query}`,
+        method: "GET",
+      };
 
-function loginSuccessCallback(data) {
-  let userId = null;
-  if (data.loggedIn) {
-    alert("Login successful");
-    sessionStorage.setItem("userId", data.userId);
-    window.location.href = "user_service/index.html";
-  } else {
-    alert("Incorrect nic or password!");
+      $.ajax(settings).done(loginSuccessCallback).fail(loginUnsuccessCallback);
   }
-}
 
-function loginUnsuccessCallback() {
-  alert("Login Unsuccessful!");
+  function loginSuccessCallback(data) {
+    let userId = null;
+    if (data.loggedIn) {
+      alert("Login successful");
+      sessionStorage.setItem("userId", data.userId);
+      window.location.href = "user_service/index.html";
+    } else {
+      alert("Incorrect nic or password!");
+    }
+  }
+
+  function loginUnsuccessCallback() {
+    alert("Login Unsuccessful!");
+  }
 }
