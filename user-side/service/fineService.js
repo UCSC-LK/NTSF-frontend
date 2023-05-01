590c396ba758c55bf9960d28e5ce9425dc1b07a1
AvishiGan <avishiganepola@gmail.com>
2023-05-01 16:08:20 +0530

    Attach authorization header into fineService

--- user-side/service/fineService.js
+++ user-side/service/fineService.js
@@ -1,15 +1,14 @@
 import { HOST_NAME } from "./constants.js";
+import { attachAuthorizationHeader } from "./jwtHandler.js";
 
-export const getFinesByNic = (nic, offenceType, callback) => {
+export const getFinesByNic = (nic, callback) => {
   const httpRequest = new XMLHttpRequest();
   httpRequest.onreadystatechange = function () {
     if (this.readyState === 4) {
       const responseBody = this.responseText;
-      const offenceType = this.getResponseHeader("offence_type");
-      const amount = this.getResponseHeader("amount");
       console.log(responseBody);
       if (this.status === 200) {
-        callback(JSON.parse(responseBody), offenceType, amount);
+        callback(JSON.parse(responseBody));
       } else {
       }
     }
@@ -22,5 +21,9 @@ export const getFinesByNic = (nic, offenceType, callback) => {
     "Content-type",
     "application/x-www-form-urlencoded"
   );
-  httpRequest.send();
+  if (attachAuthorizationHeader(httpRequest)) {
+    httpRequest.send();
+    return true;
+  }
+  return false;
 };
