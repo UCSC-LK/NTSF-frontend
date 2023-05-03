f39eda4d7d5056ca63a0569ac534db155b2c2e67
AvishiGan <avishiganepola@gmail.com>
2023-05-03 21:21:28 +0530

    Change script1.js to script.js

--- user-side/user/complaint/add/script.js
+++ user-side/user/complaint/add/script.js
@@ -1,17 +1,84 @@
-import { addComplaintByUserId } from "service/complaintService.js";
+import {
+  validateTitle,
+  validateDescription,
+} from "/user-side/util/validator.js";
 
-window.addEventListener("load", () => {
-  console.log("callback");
-  //Enter user id here
-  addComplaintByUserId(65, complaintDataHTMLinput);
-});
+/**
+ *
+ * @param {title/description} elementId
+ * @param {validateTitle/validateDescription} validateFn
+ * @returns
+ */
+function validateInputField(elementId, validateFn) {
+  const inputElement = document.getElementById(elementId);
 
-function complaintDataHTMLinput() {
-  const user_id = "65";
+  if (!validateFn(inputElement.value)) {
+    console.log(`Provided ${elementId} is invalid`);
+    inputElement.classList.add("invalid");
+    return false;
+  }
+
+  inputElement.classList.remove("invalid");
+  return true;
+}
+
+/**
+ * Validate the login form
+ */
+window.validateComplaintForm = function validateComplaintForm() {
+  const submitButtonElement = document.getElementById("submit-btn");
+  const isTitleValid = validateInputField("title", validateTitle);
+  const isDescriptionValid = validateInputField(
+    "description",
+    validateDescription
+  );
+
+  // Enable the submit button if all the input fields are valid
+  if (isTitleValid && isDescriptionValid) {
+    submitButtonElement.disabled = false;
+  } else {
+    submitButtonElement.disabled = true;
+  }
+};
+
+function addComplaint() {
+  // let user_id = sessionStorage.getItem("user_id");
+  // const user_id = "65";
+
+  // Adding session storage to store the user id
+  sessionStorage.setItem("userId", "65");
+  const userId = sessionStorage.getItem("userId");
+
+  let fineNo = document.getElementById("fineNo").value;
   let title = document.getElementById("title").value;
   let description = document.getElementById("description").value;
 
-  console.log(user_id);
-  console.log(title);
-  console.log(description);
+  let httpReq = new XMLHttpRequest();
+  httpReq.onreadystatechange = function () {
+    if (this.readyState === 4) {
+      addComplaintData(this); //This is where we get the response when the request was successfully send and a successful response was received
+    }
+  };
+
+  httpReq.open(
+    "POST",
+    `http://localhost:8080/ntsf_backend_war/complaint?action=createComplaint&user_id=${userId}&fine_no=${fineNo}&title=${title}&description=${description}`,
+    true
+  );
+  httpReq.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  httpReq.send();
+
+  function addComplaintData(httpReq) {
+    let jsonAddComplaintResponse = JSON.parse(
+      httpReq.responseText
+    ); /* Here when we receive the response from the server, we convert it to JSON format as it */
+
+    if (httpReq.status === 200) {
+      alert("Complaint Added Successfully");
+      window.location.href = "#";
+    } else {
+      alert("Complaint Not Added");
+    }
+    console.log(jsonAddComplaintResponse);
+  }
 }
