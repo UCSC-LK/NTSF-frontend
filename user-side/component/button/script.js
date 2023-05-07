fb3bd0e6454bd7d2258b6ef969fbcb441d018544
AvishiGan <avishiganepola@gmail.com>
2023-05-07 13:32:04 +0530

    Add fineNo as a parameter of displayButton function

--- user-side/component/button/script.js
+++ user-side/component/button/script.js
@@ -1,10 +1,11 @@
 /**
  * Creates and displays a button with a specified label and CSS class name
+ * fineNo -> default value = null | Not to break it's common behaviour
  * @param {string} buttonLabel - Label of the button
  * @param {string} className - The class name of the button
  * @param {function} callback - Callback function to execute when the button is clicked
  */
-export function displayButton(buttonLabel, className, callback) {
+export function displayButton(buttonLabel, className, callback, fineNo = null) {
   // Create a new button element
   const button = document.createElement("button");
 
@@ -14,8 +15,14 @@ export function displayButton(buttonLabel, className, callback) {
   // Set the text content of the button
   button.textContent = buttonLabel;
 
-  // Add an event listener for the button click
-  button.addEventListener("click", callback);
+  // Add an event listener for the button click event
+  button.addEventListener("click", () => {
+    if (fineNo != null) {
+      callback(fineNo);
+    } else {
+      callback();
+    }
+  });
 
   return button;
 }
